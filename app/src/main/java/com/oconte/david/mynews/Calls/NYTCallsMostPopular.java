cc9b73047f21d13d2cb35756da3c1ec77ca154fe
David.OA <david.oconte@orange.fr>
2020-03-25 12:17:00 +0100

    I add some ressources and clean the code for see my resultSearch VIew.

--- app/src/main/java/com/oconte/david/mynews/Calls/NYTCallsMostPopular.java
+++ app/src/main/java/com/oconte/david/mynews/Calls/NYTCallsMostPopular.java
@@ -3,6 +3,7 @@ package com.oconte.david.mynews.Calls;
 import android.support.annotation.Nullable;
 
 import com.oconte.david.mynews.Models.Result;
+import com.oconte.david.mynews.Models.SearchResult;
 import com.oconte.david.mynews.NYTFactory;
 import com.oconte.david.mynews.NYTService;
 
@@ -21,28 +22,28 @@ public class NYTCallsMostPopular {
     }
 
     // Public methode to start fetching
-    public static void getMostPopular(NYTCallsMostPopular.Callbacks callbacks, String section) {
+    public static void getSearchSection(NYTCallsSearch.Callbacks callbacks, String beginDate, String endDate, String querySection, String queryTerm, int pageNumber) {
 
         // weak reference to callback (avoid memory leaks)
-        final WeakReference<NYTCallsMostPopular.Callbacks> callbacksWeakReference = new WeakReference<NYTCallsMostPopular.Callbacks>(callbacks);
+        final WeakReference<NYTCallsSearch.Callbacks> callbacksWeakReference = new WeakReference<NYTCallsSearch.Callbacks>(callbacks);
 
         // Get Retrofit instance and the related endpoints
         NYTService nytService = NYTFactory.getRetrofit().create(NYTService.class);
 
         // The call on NYT API
-        Call<Result> call = nytService.getMostPopular(section);
+        Call<SearchResult> call = nytService.getSearchSection(beginDate,endDate,querySection, queryTerm, pageNumber);
 
         // Start the Call
-        call.enqueue(new Callback<Result>() {
+        call.enqueue(new Callback<SearchResult>() {
             @Override
-            public void onResponse(Call<Result> call, Response<Result> response) {
+            public void onResponse(Call<SearchResult> call, Response<SearchResult> response) {
 
                 // Call the proper callback used in controller mainfragment
                 if (callbacksWeakReference.get() != null) callbacksWeakReference.get().onResponse(response.body());
             }
 
             @Override
-            public void onFailure(Call<Result> call, Throwable t) {
+            public void onFailure(Call<SearchResult> call, Throwable t) {
 
                 // Call the proper callback used in controller mainfragment
                 if (callbacksWeakReference.get() != null) callbacksWeakReference.get().onFailure();
---
+++ app/src/main/java/com/oconte/david/mynews/Models/SearchResult.java
@@ -0,0 +1,46 @@
+package com.oconte.david.mynews.Models;
+
+public class SearchResult {
+
+    private ResponseSearch response;
+
+    public SearchResult() {
+    }
+
+
+    public ResponseSearch getResponse() {
+        return response;
+    }
+
+    public void setResponse(ResponseSearch response) {
+        this.response = response;
+    }
+
+    /*public List<ResponseSearch> getResponse() {
+        return response;
+    }
+
+    public void setResponse(List<ResponseSearch> response) {
+        this.response = response;
+    }
+
+    public SearchResult(List<ResponseSearch> response) {
+        this.response = response;
+    }
+
+    public List<ResponseSearch> response = new ArrayList<>();*/
+
+
+    /*public SectionSearch getResponse() {
+        return response;
+    }
+
+    public void setResponse(SectionSearch response) {
+        this.response = response;
+    }
+
+    public SearchResult() {
+    }
+
+    private SectionSearch response;*/
+}
