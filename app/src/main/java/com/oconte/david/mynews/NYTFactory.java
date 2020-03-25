f6f0e85d46fac4074ae9b9fbd2c96e593a4a0a94
David.OA <david.oconte@orange.fr>
2020-03-25 12:14:31 +0100

    I add some ressources and clean the code for see my resultSearch VIew.

---
+++ app/src/main/java/com/oconte/david/mynews/NYTFactory.java
@@ -0,0 +1,27 @@
+package com.oconte.david.mynews;
+
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class NYTFactory {
+
+    public static Retrofit getRetrofit(){
+        HttpLoggingInterceptor logging = new HttpLoggingInterceptor();
+        logging.setLevel(HttpLoggingInterceptor.Level.BODY);
+        OkHttpClient client = new OkHttpClient.Builder()
+                .addInterceptor(logging)
+                .build();
+
+        final Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://api.nytimes.com")
+                .addConverterFactory(GsonConverterFactory.create())
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                .client(client)
+                .build();
+
+        return retrofit;
+    }
+}
